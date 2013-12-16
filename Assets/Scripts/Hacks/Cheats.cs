fe4f42e52fb2cf7bc3b45ffdaeaa1776459e23fd
nms12 <nms12@ic.ac.uk>
2013-12-16 02:07:57 +0000

    final submission commit, cheats disabled, new title screen, waves and obstacle limit added

---
+++ Assets/Scripts/Hacks/Cheats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cheats : MonoBehaviour {
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            while(!SpawnController.Instance.SpawnObstacle());
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            while(!SpawnController.Instance.Spawn());
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            SpawnController.Instance.killEnemy();
+        }
+    }
+}
--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -6,17 +6,20 @@ public class SpawnController : MonoBehaviour {
     public int maxEnemies = 4;
     public int spawnTime = 3;
     public float radius = 2f;
+    public float obstacleRadius = 2f;
     public int wave = 1;
     public int waveSize = 5;
     public int spawnrate = 100;
-    public int numberOfWaves = 10;
+    public int numberOfWaves = 3;
 
     public static SpawnController Instance;
 
     public GameObject[] enemies;
+    public GameObject obstacle;
 
     private int coolDown = 0;
     private int enemyCount = 0;
+    private int obstacles = 0;
     private int enemiesKilledThisWave = 0;
 
 
@@ -48,13 +51,18 @@ public class SpawnController : MonoBehaviour {
             maxEnemies++;
             if (wave > numberOfWaves)
             {
-                Debug.Log("Next level");
+                for (int i = 0; i < 20; i++)
+                {
+                    if (SpawnObstacle()) break;
+                } 
+                wave = 0;
+                enemiesKilledThisWave = 0;
             }
-            enemiesKilledThisWave = 0;
+            
         }
     }
 
-    bool Spawn()
+    public bool Spawn()
     {
         var p = GameObject.FindGameObjectWithTag("Player");
         Vector3 position = Random.insideUnitSphere;
@@ -70,6 +78,7 @@ public class SpawnController : MonoBehaviour {
 
             GameObject e = Instantiate(enemies[i], position, transform.rotation) as GameObject;
             e.tag = "Enemy";
+            Debug.Log("Enemy spawned at"+position.x +" " +position.y);
             return true;
         }
         else
@@ -81,10 +90,53 @@ public class SpawnController : MonoBehaviour {
 
     }
 
+    public bool SpawnObstacle()
+    {
+        var p = GameObject.FindGameObjectWithTag("Player");
+        Vector3 position = Random.insideUnitSphere;
+        position = position * obstacleRadius;
+        position.z = 0;
+        var rotation = Random.rotation;
+        rotation.x = 0; rotation.y = 0;
+
+        if (p == null) { this.enabled = false; return true; }
+
+        if (Vector3.Distance(position, p.transform.position) >1 && obstacles<8)
+        {
+            var os = GameObject.FindGameObjectsWithTag("Hole");
+            if (os != null)
+            {
+                foreach (var o in os)
+                {
+                    if (Vector3.Distance(position, o.transform.position) < 1)
+                    {
+                        Debug.Log("obstacles too close to spawn");
+                        return false;
+                    }
+                }
+            }
+            Debug.Log("Obstacle Spawned at "+position.x +" " +position.y);
+            GameObject obs = Instantiate(obstacle, position, rotation) as GameObject;
+            obs.tag = "Hole";
+            obstacles++;
+            return true;
+        }
+        else
+        {
+            Debug.Log("couldn't spawn obstacle, too close to player");
+            return false;
+        }
+
+
+    }
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(transform.position, radius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, obstacleRadius);
 
     }
 
@@ -97,7 +149,10 @@ public class SpawnController : MonoBehaviour {
         }
         else
         {
-            if(Spawn()) coolDown = spawnrate * spawnTime;
+            for (int i = 0; i < 20; i++)
+            {
+                if (Spawn()) { coolDown = spawnrate * spawnTime; break; }                
+            }
         }
         
 	
